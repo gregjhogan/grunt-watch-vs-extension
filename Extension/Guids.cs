b5f65b087f34864fbb866dd6d5e6624b4bd3ecf4
active-low <gregjhogan@gmail.com>
2013-12-04 20:40:06 -0600

    Updated to VS 2013 Added searching all project items for the gruntfile instead of just root level items

---
+++ Extension/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace activelow.GruntWatchPackage
+{
+    static class GuidList
+    {
+        public const string guidGruntWatchPackagePkgString = "0651eb16-e3fb-4fdf-ac43-1060a45fda4e";
+        public const string guidGruntWatchPackageCmdSetString = "def32a09-7656-4391-bd74-76bd4652e53b";
+
+        public static readonly Guid guidGruntWatchPackageCmdSet = new Guid(guidGruntWatchPackageCmdSetString);
+    };
+}
